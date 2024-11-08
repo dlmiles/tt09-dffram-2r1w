6802d5ca1bf5d24ec369fbbfc5bb04e5bafca3d9
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-11-08 18:25:53 +0000

    Knock out 16 bytes from storage (looking for reduction on congestion)
    16x8 in a bank of 16 and a bank of 0.

--- src/project.v
+++ src/project.v
@@ -93,6 +93,10 @@ module tt_um_dlmiles_dffram32x8_2r1w (
   always @(*) begin
     // each byte here is removed from storage and remapped to zero location
     case(raddr_curr_a)
+      5'b10000:
+        raddr_xlat_a = 5'b00000;
+      5'b10001:
+        raddr_xlat_a = 5'b00000;
       5'b10010:
         raddr_xlat_a = 5'b00000;
       5'b10011:
@@ -188,6 +192,10 @@ module tt_um_dlmiles_dffram32x8_2r1w (
   always @(*) begin
     // each byte here is removed from storage and remapped to zero location
     case(raddr_curr_b)
+      5'b10000:
+        raddr_xlat_b = 5'b00000;
+      5'b10001:
+        raddr_xlat_b = 5'b00000;
       5'b10010:
         raddr_xlat_b = 5'b00000;
       5'b10011:
